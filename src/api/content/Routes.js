69f7268ab26f1e5021e4188fe5987e61de485f5c
josephmbassey <joseph.mbassey2@gmail.com>
2020-02-16 12:53:59 +0100

    added authentication middleware

--- src/api/content/Routes.js
+++ src/api/content/Routes.js
@@ -2,9 +2,9 @@ import express from 'express';
 import authMiddleware from '../middlewares/auth'
 import controller from './Controllers'
 var router = express.Router();
-router.post('/content', controller.compareContent);
-router.get('/content', controller.getAllContent);
-router.put('/content/:id', controller.recheckContent);
-router.get('/content/:id', controller.getSingleContent);
-router.delete('/content/:id', controller.deleteContent);
+router.post('/content', authMiddleware, controller.compareContent);
+router.get('/content', authMiddleware, controller.getAllContent);
+router.put('/content/:id', authMiddleware, controller.recheckContent);
+router.get('/content/:id', authMiddleware, controller.getSingleContent);
+router.delete('/content/:id', authMiddleware, controller.deleteContent);
 export default router;
